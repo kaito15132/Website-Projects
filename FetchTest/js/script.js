29a3eea4dac4739652a88b8fd726075ef7e7378b
kaito15132 <ethan15132@gmail.com>
2023-03-14 23:54:20 -0700

    Cleared old files

--- FetchTest/js/script.js
+++
@@ -1,6 +0,0 @@
-import fetch from "node-fetch";
-
-fetch("https://poe.ninja/api/data/currencyoverview?league=Sanctum&type=Currency")
-.then((response) => response.json())
-.then((data) => console.log(data));
-
